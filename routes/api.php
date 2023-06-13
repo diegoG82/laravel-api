4311f9121e25d344f97c9089b540deded4398a70
diegoG82 <diegogiardina@hotmail.it>
2023-06-13 15:11:46 +0200

    added test api

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,4 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
-Route::get ('/test' , function () {
-    return response()->json([
-        'name'=>'Diego',
-        'state'=>'Italy'
-    ]);
-  
-});
+Route::get ('projects', [ProjectController::class, 'index']);
